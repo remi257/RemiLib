3554e1cf9f14e253bf69611faf866729e5700140
Michał Remiszewski <remiszewskim@student.mini.pw.edu.pl>
2021-11-05 18:07:27 +0100

    Dragdrop example added

---
+++ RemiLib.Wpf/RemiLib.Wpf.ExampleApp/DragDrop/DragDropModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RemiLib.Wpf.Observable;
+
+namespace RemiLib.Wpf.ExampleApp.DragDrop
+{
+    public class DragDropModel
+    {
+        public DragDropModel(int number, string text)
+        {
+            Number.Value = number;
+            Text.Value = text;
+        }
+
+        public ObservableProperty<int> Number { get; set; } = new();
+        public ObservableProperty<string> Text { get; set; } = new();
+    }
+}
--- RemiLib.Wpf/RemiLib.Wpf.ExampleApp/DragDrop/DragDropVM.cs
+++ RemiLib.Wpf/RemiLib.Wpf.ExampleApp/DragDrop/DragDropVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,26 @@ namespace RemiLib.Wpf.ExampleApp.DragDrop
 {
     public class DragDropVM
     {
+        public DragDropVM()
+        {
+            ListViewCollection.Add(new DragDropModel(42, "LV Lorem"));
+            ListViewCollection.Add(new DragDropModel(11, "LV ipsum"));
+            ListViewCollection.Add(new DragDropModel(257, "LV dolor"));
+            ListViewCollection.Add(new DragDropModel(0, "LV sit"));
+
+            ItemsControlCollection.Add(new DragDropModel(42, "SP Lorem"));
+            ItemsControlCollection.Add(new DragDropModel(11, "SP ipsum"));
+            ItemsControlCollection.Add(new DragDropModel(257, "SP dolor"));
+            ItemsControlCollection.Add(new DragDropModel(0, "SP sit"));
+
+            DockPanelCollection.Add(new DragDropModel(42, "DP Lorem"));
+            DockPanelCollection.Add(new DragDropModel(11, "DP ipsum"));
+            DockPanelCollection.Add(new DragDropModel(257, "DP dolor"));
+            DockPanelCollection.Add(new DragDropModel(0, "DP sit"));
+        }
+
+        public ObservableCollection<DragDropModel> ListViewCollection { get; } = new();
+        public ObservableCollection<DragDropModel> ItemsControlCollection { get; } = new();
+        public ObservableCollection<DragDropModel> DockPanelCollection { get; } = new();
     }
 }
