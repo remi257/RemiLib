7b907a25738263b9e7757a8de12bb0e3c3971410
Michał Remiszewski <remiszewskim@student.mini.pw.edu.pl>
2021-11-18 18:46:42 +0100

    New converter added, drag drop example improved

---
+++ RemiLib.Wpf/RemiLib.Wpf.Converters/StringPrefixConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace RemiLib.Wpf.Converters
+{
+    public class StringPrefixConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is not null && parameter is string prefix)
+            {
+                if(!string.IsNullOrWhiteSpace(prefix))
+                {
+                    return prefix + value.ToString();
+                }
+                return "";
+            }
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
