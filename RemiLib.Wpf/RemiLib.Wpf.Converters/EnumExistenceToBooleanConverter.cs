da81bcf107e99469c9ce7078046ab99ae1e8dd3b
Michał Remiszewski <remiszewskim@student.mini.pw.edu.pl>
2021-11-21 15:26:34 +0100

    New converters added

---
+++ RemiLib.Wpf/RemiLib.Wpf.Converters/EnumExistenceToBooleanConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace RemiLib.Wpf.Converters
+{
+    public class EnumExistenceToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is Enum e1 && parameter is Enum e2 && e1.GetType().Equals(e2.GetType()))
+            {
+                uint u1 = System.Convert.ToUInt32(e1);
+                uint u2 = System.Convert.ToUInt32(e2);
+                return (u1 & u2) != 0; 
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RemiLib.Wpf/RemiLib.Wpf.Converters/PathToFilenameConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace RemiLib.Wpf.Converters
+{
+    public class PathToFilenameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is string s && !string.IsNullOrWhiteSpace(s))
+            {
+                return s.Split(@"\")[^1];
+            }
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
